f4c143b74c77b82ccac736c388ef6b46a5c27432
Jie Chen <jiechenmc@gmail.com>
2025-02-26 23:40:54 -0500

    save work

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div>Hi</div>
+export default async function Page() {
+  const data = await fetch('http://server:8080/')
+  const posts = await data.json()
+  console.log(posts)
+  return (
+    <ul>
+      {posts.Connected}
+    </ul>
+  )
 }
